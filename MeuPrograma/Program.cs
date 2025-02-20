67b3d764b81f0d0496c90a915ee561e33a36e673
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-02-20 20:49:44 +0000

    comentários ex2

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// open public class program
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// starts the script to write on the console hello world and bye world
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
