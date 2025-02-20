67736666ba21bb6350f48359886100af5cb3a47a
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-02-20 21:06:03 +0000

    Variaveis int ex3

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        /// <summary>
+        /// Program starts here
+        /// Multiple variables being defined and called
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            int numberone = 1;
+            int numbertwo = 2;
+            int numberfive = 5;
+
+            Console.WriteLine(numberone);
+            Console.WriteLine(numbertwo);
+            Console.WriteLine(numberfive);
+
+            
+        
+        }
+    }
+}
