1c62e5ee4880d0238da1417d5b81c55257066356
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-02-20 21:52:42 +0000

    Variaveis string ex4

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        /// <summary>
+        /// Program Starts here and i define and call many variables
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+           string ola = "Ola";
+           string eu = "Eu sou o Diogo\nTenho 18 anos\u0021";
+
+            Console.WriteLine(ola);
+            Console.WriteLine(eu);
+        }
+    }
+}
