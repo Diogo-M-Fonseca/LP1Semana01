ea02399e41a42af65545aab636210e13757a0755
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-02-21 17:33:42 +0000

    first try

---
+++ Smiley/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+
+
+namespace Smiley
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insere código:");
+            string read = Console.ReadLine();
+            int conv = int.Parse(read);
+
+            string asw = char.ConvertFromUtf32(conv);
+            Console.WriteLine(asw);
+
+        }
+    }
+}
